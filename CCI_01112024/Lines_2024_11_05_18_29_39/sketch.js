71ef93ee3303e9a7f3f701a6a813a0282fc2478a
Helin Ulas <s.helinulas@gmail.com>
2024-11-05 20:03:42 +0100

    new css

--- CCI_01112024/Lines_2024_11_05_18_29_39/sketch.js
+++ CCI_01112024/Lines_2024_11_05_18_29_39/sketch.js
@@ -4,19 +4,22 @@ let angleOffset = 0; // Initial angle
 
 function setup() {
   createCanvas(400, 400);
-  background(255); // White background
+  background(200); // White background
 
   // Start at a random position on the canvas
   x = width / 2;
   y = height / 2;
   
-  stroke(0); // Black lines
+  //stroke(0); // Black lines 
+  //change color of each line
+  stroke(random(255), random(255), random(255));
+
   strokeWeight(2); // Line thickness
 }
 
 function draw() {
   // Draw lines until reaching the canvas edges
-  if (x > 0 && x < width && y > 0 && y < height) {
+  if (x > 0 && x < width && y > 0 && y < height) { // Check if the point is within the canvas
     let angle = angleOffset + random(TWO_PI); // Random angle for each line
     let newX = x + cos(angle) * length;
     let newY = y + sin(angle) * length;
@@ -29,5 +32,13 @@ function draw() {
 
     // Slightly vary the angle offset for more randomness
     angleOffset += random(-PI / 4, PI / 4);
+    //loopp every 1000 frames
+    if (frameCount % 100 == 0) {
+      // Start at a random position on the canvas
+      x = width / 2;
+      y = height / 2;
+      angleOffset = 0;
+      background(255); // White background
+    }
   }
 }
